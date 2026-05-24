8eaf687591a00ea8222eb0c6b02ec6251ee4b66d
LeKha 521h0455 <521h0455@student.tdtu.edu.vn>
2026-05-24 11:26:00 +0700

    add sound effect

--- templates/main.js
+++ templates/main.js
@@ -10,3 +10,26 @@ glitch.addEventListener("click", () => {
     page.classList.remove("glitch-active");
   }, 1500);
 });
+
+function playSound(path, volume = 1, speed = 1) {
+
+    const sound = new Audio(path);
+
+    sound.volume = volume;
+    sound.playbackRate = speed;
+
+    sound.play();
+}
+
+function playThenGo(path, event, url) {
+  event.preventDefault();
+
+  const sound = new Audio(path);
+  sound.volume = 1;
+
+  sound.play();
+
+  setTimeout(() => {
+    window.location.href = url;
+  }, 250);
+}
